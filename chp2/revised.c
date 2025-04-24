2bd175675e3280e3cb2df33c099032372d53ea88
huang-pixel <he.huang.h@outlook.com>
2025-04-24 02:19:21 +0200

    Call by reference version

---
+++ chp2/revised.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void swap(int *pa, int *pb) {
+	int t = *pa;
+	*pa = *pb;
+	*pb = t;
+}
+
+int main() {
+	int a = 21;
+	int b = 17;
+	swap(&a, &b);
+	printf("a = %d, b = %d\n", a, b);
+	return EXIT_SUCCESS;
+}
