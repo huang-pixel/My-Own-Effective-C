e0249ddc8249cf3fad034b5545dd4dcae39da2a4
huang-pixel <he.huang.h@outlook.com>
2025-05-04 01:59:52 +0200

    First C program

---
+++ chp1/hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	puts("Hello, world!"); // puts: write a string and add a newline
+	return EXIT_SUCCESS; // EXIT_SUCCESS: return statement, replace 0
+}
