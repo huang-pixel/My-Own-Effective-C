223cd95a0c739e197e8301abb458a876086b93e4
huang-pixel <he.huang.h@outlook.com>
2025-05-04 01:24:40 +0200

    Add comments for revised version using pointers

--- chp2/revised.c
+++ chp2/revised.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void swap(int *pa, int *pb) {
-	int t = *pa;
-	*pa = *pb;
-	*pb = t;
+void swap(int *pa, int *pb) { // pa -> a: 21 pb -> b: 17
+	int t = *pa;          // t: 21
+	*pa = *pb;            // pa -> a: 17 pb -> b:17
+	*pb = t;              // pb -> b: 21 pa -> a: 17
 }
 
 int main() {
-	int a = 21;
-	int b = 17;
-	swap(&a, &b);
-	printf("a = %d, b = %d\n", a, b);
+	int a = 21;           // a = 21
+	int b = 17;           // b = 17
+	swap(&a, &b);         // Pass by reference
+	printf("a = %d, b = %d\n", a, b); // a = 17 b = 21
 	return EXIT_SUCCESS;
 }
