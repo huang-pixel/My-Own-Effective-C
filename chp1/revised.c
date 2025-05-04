b51d6b253f5e5e3d73ca30cea9334d46d1b3aded
huang-pixel <he.huang.h@outlook.com>
2025-05-04 02:17:07 +0200

    Add a if-otherwise version for hello program

---
+++ chp1/revised.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	if (puts("Hello, world!") == EOF) {
+		return EXIT_FAILURE;
+		// code here never executes
+	}
+	return EXIT_SUCCESS;
+	// code here never executes
+}
